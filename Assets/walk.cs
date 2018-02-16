1ecd94eee3b223bf7012ec21cb676f27f991dd46
marianawerneck <33205506+marianawerneck@users.noreply.github.com>
2018-02-16 16:44:25 -0200

    Initial commit

---
+++ Assets/walk.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class walk : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (x,0.0f,y);
+
+
+		
+	}
+}
